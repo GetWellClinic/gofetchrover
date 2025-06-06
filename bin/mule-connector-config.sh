e9c235c0cecd981ef439dd330b8ad287098df4ab
synergetixgroup <synergetix@gmail.com>
2025-06-05 21:58:22 -0400

    Update mule-connector-config.sh with proper REPO_URL

--- bin/mule-connector-config.sh
+++ bin/mule-connector-config.sh
@@ -97,7 +97,7 @@ EOF
 echo "Mule configuration has been saved to $output_file"
 
 # Variables
-REPO_URL="git@github.com/GetWellClinic/hl7_file_management.git"
+REPO_URL="https://github.com/GetWellClinic/hl7_file_management.git"
 CLONE_DIR="./Docker/mule"
 
 # Clone the repo into ./Docker/mule
