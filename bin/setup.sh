72edc092e98ec5953283a6857a01b7d6d0032060
Justin Joseph <justin.josephmkj@gmail.com>
2025-06-27 12:06:38 -0400

    PFX Extration files

--- bin/setup.sh
+++ bin/setup.sh
@@ -29,44 +29,11 @@ mkdir -p "$GOFETCHROVER/volumes/medhealth/files"
 
 echo "Created directories."
 
-echo "------------------------"
-echo "PFX Extraction Script"
-echo "------------------------"
 
-# Set default directory
-DEFAULT_PFX_DIR="$GOFETCHROVER/volumes/secrets"
+read -p "Do you want to extract the PFX file? (y/n): " answer
+answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
 
-# Prompt for just the filename
-read -p "Enter the name of your .pfx file (e.g. yourfile.pfx, path not required $GOFETCHROVER/volumes/secrets will be used): " PFX_NAME
-echo ""
-
-# Append to full path
-PFX_FILE="$DEFAULT_PFX_DIR/$PFX_NAME"
-
-# Check if file exists
-if [ ! -f "$PFX_FILE" ]; then
-  echo "File not found: $PFX_FILE"
-  exit 1
+if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
+  cd bin/
+  ./extract-pfx.sh
 fi
-
-# Prompt once for password (no echo)
-read -s -p "Enter PFX file password: " PFX_PASSWORD
-echo ""
-
-# Extract the private key (unencrypted with -nodes)
-echo "Extracting private key to client_key.pem..."
-openssl pkcs12 -in "$PFX_FILE" -nocerts -nodes -passin pass:"$PFX_PASSWORD" -out $GOFETCHROVER/volumes/secrets/client_key.pem
-
-# Extract the client certificate
-echo "Extracting client certificate to client_certificate.pem..."
-openssl pkcs12 -in "$PFX_FILE" -clcerts -nokeys -passin pass:"$PFX_PASSWORD" -out $GOFETCHROVER/volumes/secrets/client_certificate.pem
-
-# Extract the root/CA certificate(s)
-echo "Extracting CA certificate(s) to root_certificate.pem..."
-openssl pkcs12 -in "$PFX_FILE" -cacerts -nokeys -passin pass:"$PFX_PASSWORD" -out $GOFETCHROVER/volumes/secrets/root_certificate.pem
-
-echo ""
-echo "Extraction complete. Files saved in:"
-echo " - $GOFETCHROVER/volumes/secrets/client_key.pem"
-echo " - $GOFETCHROVER/volumes/secrets/client_certificate.pem"
-echo " - $GOFETCHROVER/volumes/secrets/root_certificate.pem"
