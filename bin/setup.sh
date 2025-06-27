8db277d60bea8fb6094497bc71652450682bf19a
Justin Joseph <justin.josephmkj@gmail.com>
2025-06-27 12:21:39 -0400

    Time zone update

--- bin/setup.sh
+++ bin/setup.sh
@@ -10,11 +10,14 @@
 cd ..
 export GOFETCHROVER=$(pwd)
 
+echo ""
 echo "GOFETCHROVER is set to: $GOFETCHROVER"
+echo ""
 
 
 # Start directory creation
 echo "Creating directories..."
+echo ""
 sleep 1s
 
 # Create directory structure
@@ -28,7 +31,35 @@ mkdir -p "$GOFETCHROVER/volumes/alphalab/files"
 mkdir -p "$GOFETCHROVER/volumes/medhealth/files"
 
 echo "Created directories."
+echo ""
 
+# Update timezoneMore actions
+echo "Update your correct timezone to synchronize logs..."
+echo ""
+
+read -p "Do you want to configure your timezone now? (y/n): " answer
+answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
+    /usr/bin/tzselect
+fi
+
+# Backup with datestamp previous JSON config
+echo "Backing up existing JSON config files..."
+sleep 1s
+cp "$GOFETCHROVER/volumes/rover/rover_config.json" "$GOFETCHROVER/volumes/rover/rover_config.json.$(date +'%Y-%m-%d')"
+cp "$GOFETCHROVER/volumes/dcare/dynacare_config.json" "$GOFETCHROVER/volumes/dcare/dynacare_config.json.$(date +'%Y-%m-%d')"
+cp "$GOFETCHROVER/volumes/alphalab/alphalab_config.json" "$GOFETCHROVER/volumes/alphalab/alphalab_config.json.$(date +'%Y-%m-%d')"
+cp "$GOFETCHROVER/volumes/medhealth/medhealth_config.json" "$GOFETCHROVER/volumes/medhealth/medhealth_config.json.$(date +'%Y-%m-%d')"
+
+# Create JSON from templateMore actions
+echo "Creating new JSON configuration files from templates..."
+sleep 1s
+cp "$GOFETCHROVER/volumes/rover/rover_config.json.example" "$GOFETCHROVER/volumes/rover/rover_config.json"
+cp "$GOFETCHROVER/volumes/dcare/dynacare_config.json.example" "$GOFETCHROVER/volumes/dcare/dynacare_config.json"
+cp "$GOFETCHROVER/volumes/alphalab/alphalab_config.json.example" "$GOFETCHROVER/volumes/alphalab/alphalab_config.json"
+cp "$GOFETCHROVER/volumes/medhealth/medhealth_config.json.example" "$GOFETCHROVER/volumes/medhealth/medhealth_config.json"
+echo ""
 
 read -p "Do you want to extract the PFX file? (y/n): " answer
 answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
