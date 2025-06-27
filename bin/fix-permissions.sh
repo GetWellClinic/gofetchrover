31b08f8af1f8bd5b3224d42fe479ab942ecf634b
Justin Joseph <justin.josephmkj@gmail.com>
2025-06-27 13:08:42 -0400

    Fix permissions script

--- bin/fix-permissions.sh
+++ bin/fix-permissions.sh
@@ -10,6 +10,8 @@ echo "GOFETCHROVER is set to: $GOFETCHROVER"
 # Find all .sh files in GOFETCHROVER and set permissions to 750
 echo "Setting permissions to 750 for all .sh files in $GOFETCHROVER..."
 sudo find "$GOFETCHROVER" -type f -name "*.sh" -exec chmod 550 {} \;
+sudo find "$GOFETCHROVER" -type f -name "*.json" -exec chmod 664 {} \;
+sudo find "$GOFETCHROVER" -type f -name "*.py" -exec chmod 755 {} \;
 sudo chmod 550 mule
 sudo chmod 550 gofetch
 sudo chmod 550 fetchnow
